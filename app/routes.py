9f0f08e7028410c2c34457b4e596ec770e901066
Kai-Bin Wong <wongkaibin8080@gmail.com>
2026-06-15 21:26:39 +1000

    updated routes

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, render_template
+from . import models
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
+    items = models.get_all_items_with_metadata()
+    return render_template('index.html', items=items)
 
-# add about page
+# add about page, no helper needed
 # @main_bp.route('/about')
 # def about():
 #     return render_template('about.html')
 
 @main_bp.route('/item/<int:item_id>')
 def item_detail(item_id):
-    return render_template('item_details.html', id=item_id)
+    item = models.get_item_with_metadata(item_id)
+    return render_template('item_details.html', item=item)
 
 @main_bp.route('/assessment/<int:item_id>')
 def item_assessment(item_id):
-    return render_template('item_assessment.html', id=item_id)
+    item = models.get_item_with_metadata(item_id)
+    return render_template('item_assessment.html', item=item)
