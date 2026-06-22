26f7dec720b44891aeee8156245373dda9e1bef9
Kai-Bin Wong <wongkaibin8080@gmail.com>
2026-06-22 21:33:24 +1000

    updated init and routes, added error.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
 mysql =MySQL()
@@ -10,5 +10,13 @@ def create_app():
     
     from .routes import main_bp
     app.register_blueprint(main_bp)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('error.html', code=404, message='Page not found'), 404
+    
+    @app.errorhandler(500)
+    def internal_error(e):
+        return render_template('error.html', code=500, message='Internal Server Error'), 500
     
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,44 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from . import models
+from functools import wraps
 
 main_bp = Blueprint('main', __name__)
 
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('userID'):
+            return redirect(url_for('main.login'))
+        if session.get('userRole') != 'Admin':
+            return render_template('error.html',
+                                    code=403,
+                                    message='Access denied. Admin privileges required'), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+def elder_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('userID'):
+            return redirect(url_for('main.login'))
+        if session.get('userRole') not in ('Elder', 'Admin'):
+            return render_template('error.html'
+                                    code=403,
+                                    message='Access denied. Elder or Admin privileges required.'), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+def curator_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('userID'):
+            return redirect(url_for('main.login'))
+        if session.get('userRole') not in ('Curator', 'Admin'):
+            return render_template('error.html',
+                                    code=403,
+                                    message='Access denied. Curator or Admin privileges required.'), 403
+        return f(*args, **kwargs)
+    return decorated_function
 
 @main_bp.route('/')
 def index():
@@ -71,6 +107,7 @@ def item_detail(item_id):
 
 
 @main_bp.route('/assessment/<int:item_id>')
+@elder_required
 def item_assessment(item_id):
     item = models.get_item_with_metadata(item_id)
     return render_template('item_assessment.html', item=item)
